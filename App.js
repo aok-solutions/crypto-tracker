7cde86aa242cbd5eb6cfcb10a0f7a8e0eecd36e7
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2018-07-04 14:26:42 -0400

    create search box

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>CryptoTracker</Text>
+        <TextInput
+          style={styles.input}
+          placeholder = "Search for Crypto..."
+          />
       </View>
     );
   }
@@ -14,8 +17,20 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'center',
+    padding: 50
   },
+
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: '#fff',
+    borderBottomColor: '#ddd',
+    borderWidth: 2,
+    paddingLeft: 15,
+    paddingRight: 15
+  }
 });
