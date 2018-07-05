1a4f523a5a8ee3947d89d0aa8ea3c45e09f9c8c3
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2018-07-04 23:51:09 -0400

    fetch coin list

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { StyleSheet, TextInput, View, FlatList } from 'react-native'
 import { List, ListItem, FormInput } from 'react-native-elements'
+import axios from 'axios'
 
 export default class App extends React.Component {
   constructor(props) {
       super(props)
       this.state = {
-        cryptoList: [
-          {name: 'BTC'},
-          {name: 'ETH'},
-          {name: 'ZRX'}
-        ]
+        cryptoList: []
       }
+  }
+
+  fetchCrypto = () => {
+    const url = 'https://min-api.cryptocompare.com/data/all/coinlist'
+    axios.get(url)
+      .then((response) => {
+        let coinList = Object.entries(response.data.Data).map(coin => {
+          return {name: coin[1].FullName}
+        })
+
+        this.setState({
+          cryptoList: coinList
+        })
+      })
+      .catch((error) => console.log('error fetching coin list: ', error))
   }
 
   renderSeparator = () => <View style={styles.separator}/>
@@ -22,6 +34,7 @@ export default class App extends React.Component {
         <FormInput
           placeholder="Search for crypto..."
           containerStyle={{ marginBottom: 20, marginTop: 50 }}
+          onFocus={this.fetchCrypto}
         />
         <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
           <FlatList
